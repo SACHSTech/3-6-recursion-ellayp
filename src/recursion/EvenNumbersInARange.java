aa81a3b74f37b6b709079920b98a15d48ddcd854
ellayp <ella.yap26@ycdsbk12.ca>
2025-05-16 11:29:02 -0400

    Completed Tier 2: Strings and Numbers Reverse a String

--- src/recursion/EvenNumbersInARange.java
+++ src/recursion/EvenNumbersInARange.java
@@ -2,14 +2,19 @@ package recursion;
 
 public class EvenNumbersInARange {
     public static void main(String[] args) {
-        printEvens(0, 10); // Example range
+        printEvens(0, 0); // Output: 0
         System.out.println();
-        printEvens(1, 11); // Example range
+        printEvens(0, 10); // Output: 0 2 4 6 8 10
+        System.out.println();
+        printEvens(1, 11); // Output: 2 4 6 8 10
     }
 
     public static void printEvens(int start, int end) {
         for (int i = start; i <= end; i++) {
-            if (i % 2 == 0) {
+            if (start == 0 && end == 0) {
+                System.out.print(0);
+            }
+            else if (i % 2 == 0) {
                 System.out.print(i + " ");
             }
             else {
---
+++ src/recursion/ReverseAString.java
@@ -0,0 +1,21 @@
+package recursion;
+
+public class ReverseAString {
+    public static void main(String[] args) {
+        reverse(""); // Output:
+        System.out.println();
+        reverse("racecar"); // Output: racecar
+        System.out.println();
+        reverse("hello"); // Output: olleh
+    }
+
+    public static void reverse(String s) {
+        if (s.isEmpty()) {
+            return;
+        } else {
+            System.out.print(s.charAt(s.length() - 1));
+            reverse(s.substring(0, s.length() - 1));
+        }
+    }
+    
+}
